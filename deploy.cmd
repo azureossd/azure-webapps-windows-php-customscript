fb0d6384f7d5868569fd03b73a6aac91782c63e7
Edison Garcia Chinas <edisga@microsoft.com>
2020-09-24 19:12:54 -0500

    Initial Commit

--- deploy.cmd
+++ deploy.cmd
@@ -73,10 +73,16 @@ goto :EOF
 
 echo PHP deployment
 
-:: 1. Check for Composer ARGS
+:: 1. KuduSync
+IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  IF !ERRORLEVEL! NEQ 0 goto error
+)
+
+:: 2. Check for Composer ARGS
 call :InitializeDeploymentConfig
 
-:: 2. Use composer
+:: 3. Use composer
 echo "%DEPLOYMENT_TARGET%"
 IF EXIST "%DEPLOYMENT_TARGET%\composer.json" (
    echo "Found composer.json"
@@ -84,13 +90,6 @@ IF EXIST "%DEPLOYMENT_TARGET%\composer.json" (
    call :ExecuteCmd php composer.phar install "%COMPOSER_ARGS%"
    IF !ERRORLEVEL! NEQ 0 goto error
    popd
-)
-
-
-:: 3. KuduSync
-IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-  IF !ERRORLEVEL! NEQ 0 goto error
 )
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
